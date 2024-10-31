ba2d7f2260d56e4e87b82c557085b62e54f6767e
Roman Sadovskiy <s03roman@mail.ru>
2024-10-31 20:58:48 +0300

    desktop

---
+++ MassCultureLibrary.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using MassCultureLibrary.Desktop.Pages;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MassCultureLibrary.Desktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static MainWindow Instance;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Instance = this;
+            MainFrame.Navigate(new MainPage());
+        }
+    }
+}
