03c3b1a898794b5622d3a6c03a3fbfe41259451f
kkerra <fastergradanna@yandex.ru>
2024-11-01 14:12:04 +0300

    new test

---
+++ MassCultureLibrary/Games/GameService.cs
@@ -0,0 +1,39 @@
+﻿namespace MassCultureLibrary.Games
+{
+    public class GameService : IGameService
+    {
+        IGameRepository _repository;
+
+        public GameService(IGameRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Game> AddGameAsync(Game game)
+        {
+            if (game == null)
+                throw new ArgumentException("Game is null");
+            return await _repository.AddAsync(game);
+        }
+
+        public Task DeleteGameAsync(Guid gameId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Game?> GetGameByIdAsync(Guid gameId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Game>> GetGamesByPlatformAsync(string platform)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Game> UpdateGameAsync(Guid gameId, GameUpdateDto updateInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
