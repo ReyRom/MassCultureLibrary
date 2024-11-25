98cfcf7e3d9a21d71a506e1155bf6027c291c63c
221 <221@PC-Klass.ru>
2024-11-25 15:02:27 +0300

    labwork12

--- MassCultureLibrary.Desktop/Pages/MainPage.xaml.cs
+++ MassCultureLibrary.Desktop/Pages/MainPage.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MassCultureLibrary.Animes;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MassCultureLibrary.Desktop.Pages
 {
@@ -20,14 +9,27 @@ namespace MassCultureLibrary.Desktop.Pages
     /// </summary>
     public partial class MainPage : Page
     {
+        private readonly AnimeService _animeService = new(new JsonAnimeStorage());
+
         public MainPage()
         {
             InitializeComponent();
+            UpdateAnimeList();
         }
 
-        private void AnimeButton_Click(object sender, RoutedEventArgs e)
+        private async void UpdateAnimeList()
         {
-            MainWindow.Instance.MainFrame.Navigate(new AnimeListPage());
+            AnimeDataGrid.Items.Clear();
+            var animes = await _animeService.GetAnimeAsync();
+            foreach (var anime in animes)
+                AnimeDataGrid.Items.Add(anime);
+        }
+
+        private async void AddGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            Anime anime = new() { Id = Guid.NewGuid(), Title = AnimeNameTextBox.Text, Genre = AnimeGenreTextBox.Text, Status = AnimeStatusTextBox.Text };
+            await _animeService.AddAnimeAsync(anime);
+            UpdateAnimeList();
         }
     }
 }
